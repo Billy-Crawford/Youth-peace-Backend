2aab5178e282cddadd9938df65dd3a7c58079de4
Billy-Craweford <oumaroubilly6@gmail.com>
2026-06-02 03:11:09 +0200

    prepare production settings for render

--- config/settings.py
+++ config/settings.py
@@ -12,10 +12,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-g+j^sd+#cr=e$@^!ewy^yh!nu9_95fh&qnfppo4%u0ll=)nat%"
+
+SECRET_KEY = os.getenv("SECRET_KEY", "fallback-dev-key")
+# SECRET_KEY = "django-insecure-g+j^sd+#cr=e$@^!ewy^yh!nu9_95fh&qnfppo4%u0ll=)nat%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = os.getenv("DEBUG", "False") == "True"
+# DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
