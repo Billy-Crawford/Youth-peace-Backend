b2b3b81761b946de9f13d14004e80616269530d4
Billy-Craweford <oumaroubilly6@gmail.com>
2026-06-03 17:47:07 +0200

    localisation auto

--- alerts/serializers.py
+++ alerts/serializers.py
@@ -1,7 +1,6 @@
-# alerts/serializers.py
+# # alerts/serializers.py
 
 from rest_framework import serializers
-
 from .models import AlertReport
 
 
@@ -11,7 +10,6 @@ class AlertReportSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = AlertReport
-
         fields = (
             "id",
             "reporter",
@@ -38,10 +36,65 @@ class AlertReportSerializer(serializers.ModelSerializer):
         )
 
     def get_reporter_name(self, obj):
-        return (
-            f"{obj.reporter.first_name} "
-            f"{obj.reporter.last_name}"
-        )
+        return f"{obj.reporter.first_name} {obj.reporter.last_name}"
+
+    def validate(self, data):
+        # DRF validation correcte
+        lat = data.get("latitude")
+        lng = data.get("longitude")
+
+        if lat is None or lng is None:
+            raise serializers.ValidationError(
+                "La localisation (latitude/longitude) est requise."
+            )
+
+        return data
 
 
 
+# from rest_framework import serializers
+#
+# from .models import AlertReport
+#
+#
+# class AlertReportSerializer(serializers.ModelSerializer):
+#
+#     reporter_name = serializers.SerializerMethodField()
+#
+#     class Meta:
+#         model = AlertReport
+#
+#         fields = (
+#             "id",
+#             "reporter",
+#             "reporter_name",
+#             "title",
+#             "description",
+#             "report_type",
+#             "location_name",
+#             "latitude",
+#             "longitude",
+#             "photo",
+#             "status",
+#             "created_at",
+#             "updated_at",
+#         )
+#
+#         read_only_fields = (
+#             "id",
+#             "reporter",
+#             "reporter_name",
+#             "status",
+#             "created_at",
+#             "updated_at",
+#         )
+#
+#     def get_reporter_name(self, obj):
+#         return (
+#             f"{obj.reporter.first_name} "
+#             f"{obj.reporter.last_name}"
+#         )
+#
+#
+#
+#
